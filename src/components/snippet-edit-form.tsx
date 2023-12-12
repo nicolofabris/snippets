46a1093ba7440092d530f07e6ae69ab56d4f01ac
nicolofabris <nicolo.fabriss@gmail.com>
2023-12-12 13:13:49 +0100

    server form + error handling

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -3,6 +3,7 @@
 import type { Snippet } from "@prisma/client";
 import Editor from "@monaco-editor/react";
 import { useState } from "react";
+import * as actions from '@/actions';
 
 
 interface SnippetEditFormProps {
@@ -17,6 +18,8 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         setCode(value)
     }
 
+    const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code)
+
   return (
     <div>
       <Editor
@@ -27,6 +30,9 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         options={{minimap: {enabled: false}}}
         onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+        <button type="submit" className='p-2 border rounded'>Save</button>
+      </form>
     </div>
   );
 }
